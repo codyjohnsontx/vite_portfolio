56331c3add8cdfd43df3acca8f97dba8faf281b7
Cody Johnson <codyjohnsontx@gmail.com>
2024-10-08 12:30:46 -0500

    update work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,44 +1,51 @@
 import React from 'react'
 import WorkItem from './WorkItem'
 // WorkItem above might need to change to just Work
-const data =[
+const data = [
     {
-        year: "2023 - present",
+        year: "Jul 2024 - Present",
+        company: "Diaz Martial Arts",
+        title: 'Software Engineer/Brazilian Jiu-Jitsu Instructor',
+        details: "Developed instructional and management skills by leading classes and managing administrative tasks, while building a SPA for Crazy8's Grappling Club using React/Vite, which increased attendance by 14% in 3 months. Currently prototyping and wireframing an educational VOD app to deliver content for affiliate schools."
+    },
+    {
+        year: "Aug 2023 - Present",
+        company: "Ducati Austin",
+        title: 'Software Engineer',
+        details: 'Pioneered the ongoing transition to a more dynamic and responsive website platform utilizing Shopify and later a custom built React site. Played a pivotal role in designing and constructing innovative website mock-ups, setting the stage for a comprehensive website overhaul aimed at enhancing user experience and optimizing operational efficiency.',
+    },
+    {
+        year: "Jan 2023 - Present",
         company: '100 Devs',
         title: 'Software Engineer',
-        details: 'Collaborated with a dynamic team to design and implement modern, responsive web applications, ensuring adherence to industry standards and best practices. Leveraging my expertise in full-stack development, I focused on optimal code organization and maintainability for long-term scalability, while actively contributing to agile methodologies such as SCRUM for efficient project management and delivery.'
+        details: 'Collaborated with developers and designers to build modern, responsive web applications, applying industry best practices and agile methodologies. Leveraged full-stack expertise to design scalable, maintainable solutions and contributed to educational open-source projects that make technical knowledge accessible to aspiring developers.'
     },
     {
-        year: "2023 - present",
-        company: "Ducati Austin",
-        title: 'Freelancer',
-        details: 'Pioneered the ongoing transition to a more dynamic and responsive website platform utilizing Shopify. Played a pivotal role in designing and constructing innovative website mock-ups, setting the stage for a comprehensive website overhaul aimed at enhancing user experience and optimizing operational efficiency.',
-    },
-    {
-        year: "2021 - present",
+        year: "Apr 2021 - Present",
         company: "Texas Malibu",
-        title: 'Sales Associate',
-        details: 'Facilitated seamless boat purchases for clients, managed custom orders for timely delivery, and cultivated a strong brand culture, leading to increased customer satisfaction, loyalty, and 10% growth in new client acquisition. Additionally, collaborated with management to optimize the company website, improving inventory management and user engagement.',
-    },
-    {
-        year: "2019 - 2021",
-        company: "Motovation USA",
         title: 'Content & Fulfillment specialist',
+        details: "Implemented digital inventory management processes across three stores, boosting efficiency and customer retention while expanding Malibu boat's market share in central Texas from 30% to 40.1%. Trained junior sales associates during onboarding, providing guidance on procedures, product knowledge, and sales techniques for seamless integration and success.",
+    },
+
+    {
+        year: "Nov 2019 - Apr 2021",
+        company: "Motovation USA",
+        title: 'Sales Associate/CMS',
         details: "Led digital marketing initiatives, including social media campaigns and order fulfillment, to enhance customer experience and drive sales growth. Implemented dropshipping strategies for efficient inventory management, expanded product offerings, and optimized social media content to boost engagement and sales. Additionally, strategically maintained the company's website and inventory for seamless operations, while capturing high-quality product photography to increase website traffic and improve conversion rates.",
     },
 ]
 
-const Work = () =>{
+const Work = () => {
     return (
         <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
             <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
-            {data.map((item, idx) =>(
-                <WorkItem 
-                key={idx} 
-                year={item.year} 
-                company={item.company} 
-                title={item.title} 
-                details={item.details}
+            {data.map((item, idx) => (
+                <WorkItem
+                    key={idx}
+                    year={item.year}
+                    company={item.company}
+                    title={item.title}
+                    details={item.details}
                 />
             ))}
         </div>
