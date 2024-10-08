c56e584e1ce9798e619cf486400f98b881b0edea
Cody Johnson <codyjohnsontx@gmail.com>
2024-10-08 12:34:03 -0500

    update work.jsx descriptions, spell and grammar check

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,14 +23,14 @@ const data = [
     {
         year: "Apr 2021 - Present",
         company: "Texas Malibu",
-        title: 'Content & Fulfillment specialist',
+        title: 'Sales Associate',
         details: "Implemented digital inventory management processes across three stores, boosting efficiency and customer retention while expanding Malibu boat's market share in central Texas from 30% to 40.1%. Trained junior sales associates during onboarding, providing guidance on procedures, product knowledge, and sales techniques for seamless integration and success.",
     },
 
     {
         year: "Nov 2019 - Apr 2021",
         company: "Motovation USA",
-        title: 'Sales Associate/CMS',
+        title: 'CMS/E-commerce Specialist',
         details: "Led digital marketing initiatives, including social media campaigns and order fulfillment, to enhance customer experience and drive sales growth. Implemented dropshipping strategies for efficient inventory management, expanded product offerings, and optimized social media content to boost engagement and sales. Additionally, strategically maintained the company's website and inventory for seamless operations, while capturing high-quality product photography to increase website traffic and improve conversion rates.",
     },
 ]
